ee76879e91d05ec03351b434872005765e36a281
muhammadeko <muhammadekoprasetyo29@gmail.com>
2022-09-10 08:36:00 +0700

    fix api not included

---
+++ app/server/src/api/todos.ts
@@ -0,0 +1,26 @@
+import { API } from 'server-web'
+
+export default [
+  '/api/todos/:id?',
+  async ({ body, reply, req }) => {
+    const { method } = req
+    const data = req.body
+    const id = Number(req.params.id)
+    switch (method) {
+      case 'GET':
+        isNaN(id) ? reply.send({todos:await db.todos.findMany()}) : reply.send({todo:await db.todos.findUnique({where:{id}})})
+        break
+      case 'POST':
+        reply.send({todo:await db.todos.create({data})})
+        break
+      case 'PUT':
+        delete data.id // id is not allowed to be changed
+        reply.send({todo:await db.todos.update({where:{id},data})})
+        break
+      case 'DELETE':
+        reply.send({todo:await db.todos.delete({where:{id}})})
+        break
+    }
+  },
+] as API
+      
