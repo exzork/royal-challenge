69b2c56cf0eb7bfaabf9f5669fefddd33f9810e7
Muhammad Eko Prasetyo <muhammadekoprasetyo29@gmail.com>
2022-09-30 12:32:27 +0700

    progress 30/9

--- app/web/src/base/page/ngecat.tsx
+++ app/web/src/base/page/ngecat.tsx
@@ -3,13 +3,16 @@ import { page } from 'web-init'
 import injectScript from 'src/utils/injectScript'
 import 'mirada'
 import { useEffect, useRef, useState } from 'react'
+
 export default page({
   url: '/ngecat',
   component: ({}) => {
-    const imageCanvas = useRef<HTMLImageElement>(null)
+    const imageCanvas = useRef<HTMLCanvasElement>(null)
     const newCanvas = useRef<HTMLCanvasElement>(null)
     const [openCVLoaded, setOpenCVLoaded] = useState(false)
     const [imageLoaded, setImageLoaded] = useState(false)
+    const [file, setFile] = useState<File>()
+    const [color, setColor] = useState('red')
 
     useEffect(() => {
       if (!openCVLoaded) {
@@ -31,21 +34,62 @@ export default page({
         const image = cv.imread(imageCanvas.current!)
         cv.imshow('new', image)
         image.delete()
-        imageCanvas.current!.style.display = 'none'
       }
     }, [imageLoaded, openCVLoaded])
 
+    useEffect(() => {
+      if (file != undefined) {
+        const reader = new FileReader()
+        reader.onload = (e) => {
+          if (e.target?.result) {
+            const img = new Image()
+            img.src = e.target.result.toString()
+            img.onload = () => {
+              const canvas = imageCanvas.current!
+              canvas.width = img.width
+              canvas.height = img.height
+              const ctx = canvas.getContext('2d')!
+              ctx.drawImage(img, 0, 0)
+              setImageLoaded(true)
+              const canvas2 = newCanvas.current!
+              canvas2.width = img.width
+              canvas2.height = img.height
+              const ctx2 = canvas2.getContext('2d')!
+              ctx2.drawImage(img, 0, 0)
+            }
+          }
+        }
+        reader.readAsDataURL(file)
+      }
+    }, [file])
+
+    useEffect(() => {
+      console.log(color)
+    }, [color])
+
+    const getColor = (color: string) => {
+      const codes = color.replace("#","").split('')
+      const r = parseInt(codes[0] + codes[1], 16)
+      const g = parseInt(codes[2] + codes[3], 16)
+      const b = parseInt(codes[4] + codes[5], 16)
+      return new cv.Scalar(r, g, b)
+    }
 
     newCanvas.current?.addEventListener('mousedown', (e) => {
+      const rect = newCanvas.current?.getBoundingClientRect()
+      const scaleX = newCanvas.current?.width! / rect?.width!
+      const scaleY = newCanvas.current?.height! / rect?.height!
+      const x = (e.clientX - rect?.left!) * scaleX
+      const y = (e.clientY - rect?.top!) * scaleY
+
       const image = cv.imread(newCanvas.current!)
-      const img = image
       const rgb = new cv.Mat()
       cv.cvtColor(image, rgb, cv.COLOR_RGBA2RGB)
       const gray = new cv.Mat()
       cv.cvtColor(rgb, gray, cv.COLOR_RGB2GRAY)
       const cannyGray = new cv.Mat()
-      const mask = cv.matFromArray(Math.floor(rgb.cols / 8), Math.floor(rgb.rows / 8), cv.CV_8U, new Array(Math.floor(rgb.cols / 8) * Math.floor(rgb.rows / 8)).fill(0))
-      cv.Canny(gray, cannyGray, 75, 30,3)
+      const mask = cv.matFromArray(3, 3, cv.CV_8U, [0, -1, 0, -1, 5, -1, 0, -1, 0])
+      cv.Canny(gray, cannyGray, 75, 30, 3)
 
       const hsv = new cv.Mat()
       cv.cvtColor(rgb, hsv, cv.COLOR_RGB2HSV)
@@ -58,17 +102,15 @@ export default page({
       cv.medianBlur(sChannel, sChannel, 3)
 
       const canny = new cv.Mat()
-      cv.Canny(sChannel, canny, 75, 30,3)
-      cv.addWeighted(canny, 0.5, cannyGray, 0.5, 0, canny)
+      cv.Canny(sChannel, canny, 75, 30, 3)
+      cv.addWeighted(canny, 0.7, cannyGray, 0.2, 0, canny)
       cv.dilate(canny, canny, mask, new cv.Point(-1, -1), 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue())
 
-      const seedPoint = new cv.Point(e.offsetX, e.offsetY)
+      const seedPoint = new cv.Point(x, y)
       cv.resize(canny, canny, new cv.Size(canny.cols + 2, canny.rows + 2))
-      cv.medianBlur(rgb, rgb, 15)
-      cv.floodFill(rgb, canny, seedPoint, new cv.Scalar(255, 0, 0), new cv.Rect(), new cv.Scalar(5, 5, 5), new cv.Scalar(5, 5, 5),8)
+      cv.floodFill(rgb, canny, seedPoint, getColor(color), new cv.Rect(), new cv.Scalar(5, 5, 5), new cv.Scalar(5, 5, 5), 8)
 
-      cv.dilate(rgb, rgb,mask, new cv.Point(-1, -1), 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue())
-
+      //cv.dilate(rgb, rgb,mask, new cv.Point(-1, -1), 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue())
 
       cv.imshow('new', rgb)
       image.delete()
@@ -76,13 +118,35 @@ export default page({
     })
 
 
-
     return (
-      <>
-        <img id='imageSrc' className='' ref={imageCanvas} src='/images/tembok.jpg'
-             onLoad={() => setImageLoaded(true)} />
-        <canvas id='new' className='' ref={newCanvas} />
-      </>
+      <div className='min-h-screen'>
+        {openCVLoaded ? (
+          <>
+            <div className='grid grid-cols-2 mx-12 gap-x-12  items-center justify-center'>
+              <input type='file' onChange={(e) => {
+                setFile(e.target.files![0])
+              }} />
+              <input type='color' value={color} onChange={(e) => {
+                setColor(e.target.value)
+              }} />
+            </div>
+            <div className='grid grid-cols-2 mx-12 gap-x-12 items-center justify-center'>
+              <div className='flex flex-col'>
+                <div>Original</div>
+                {/*<img id='imageSrc' className='object-contain' ref={imageCanvas} src='/images/tembok3.webp'*/}
+                {/*     onLoad={() => setImageLoaded(true)} />*/}
+                <canvas id='imageSrc' className='object-contain' ref={imageCanvas} />
+              </div>
+              <div className='flex flex-col'>
+                <div>Result</div>
+                <canvas id='new' className='object-contain' ref={newCanvas} />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div>Loading OpenCV....</div>
+        )}
+      </div>
     )
   },
 })
